31fef3bb53de8b340012ec39c352fe5701e37c3b
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-18 10:47:08 +0300

    JSX syntax

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 console.log(React);
 
-const state = `Hello`;
-
-const h1 = React.createElement('h1', {}, `${state}, react!`);
-console.log(h1);
-
 console.log(ReactDOM);
 
+const component = (data) => {
+    const h2 = React.createElement('h2', {}, data.heading); // <h2>{data.heading}</h2> --- JSX
+    const p = React.createElement('p', {}, 'Lorem ipsum'); //<p>Lorem ipsum</p> --- JSX
+    return React.createElement('div', {}, h2, p);
+}
+
+const elem = React.createElement(component, {heading: 'Heading text'});
+const elem2 = React.createElement(component,  {heading: 'Hello to user'});
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(h1, root)
+ReactDOM.render(elem2, root)
