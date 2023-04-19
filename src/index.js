2f57a3df41a1cd82e74f46f54491aa39b2e72085
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-19 09:18:58 +0300

    Syntax

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+
+
+const state = 1;
+
+//const App = React.createElement('div', {data: 'value'}, 'Text');  // <div>Text</div>
+
+const App = <div data="value">{state}</div>
+
+console.log(App);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  App
 );
 
