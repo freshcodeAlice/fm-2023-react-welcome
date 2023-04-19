4538038d263b8f1e7dea41f4a75fcbb1b896e9ce
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-19 09:35:28 +0300

    Functional and class components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 
-const App = () => {
+// Functional component
+
+/*
+const funApp = (props) => {
   return (
     <h1>Hello, <span>react!</span></h1>
   );
 }
 
+console.log(<funApp />)
+
+*/
+
+import React from "react";
+
+
+class App extends React.Component {
+  
+  render(){
+    return (
+      <section>
+        Hello, class Component!
+      </section>
+    )
+  }
+}
+
+console.log(<App />);
+
+
+
+
 export default App;
+
+
+/*
+Компонента - це функція, яка приймає вхідні дані (пропси) і повертає реакт-елементи
+
+
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-
-
-const state = 1;
-
-//const App = React.createElement('div', {data: 'value'}, 'Text');  // <div>Text</div>
-
-const App = <div data="value">{state}</div>
-
-console.log(App);
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  App
+  <App />
 );
 
