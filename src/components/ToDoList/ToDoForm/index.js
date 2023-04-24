189516ca674703ee0417e2d55570f4f1c5cc5637
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-24 10:09:49 +0300

    date validation

--- src/components/ToDoList/ToDoForm/index.js
+++ src/components/ToDoList/ToDoForm/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {format} from 'date-fns';
 import styles from './ToDoForm.module.css';
+import {isBefore} from 'date-fns';
 import cx from 'classnames';
 
 class ToDoForm extends Component {
@@ -10,7 +11,8 @@ class ToDoForm extends Component {
             todoBody: '',
             isDone: false,
             deadline: new Date(),
-            isInputValid: true
+            isInputValid: true,
+            isDateValid: true
         }
     }
     
@@ -43,6 +45,15 @@ class ToDoForm extends Component {
                  break;
             }
             case 'datetime-local': {
+                if(isBefore(new Date(target.value), new Date())) {
+                    this.setState({
+                        isDateValid: false
+                    })
+                } else {
+                    this.setState({
+                        isDateValid: true
+                    })
+                }
                 this.setState({
                  deadline: new Date(target.value)
               });
@@ -65,10 +76,15 @@ class ToDoForm extends Component {
 
 
     render() {
-        const {todoBody, isDone, deadline, isInputValid} = this.state;
+        const {todoBody, isDone, deadline, isInputValid, isDateValid} = this.state;
         const cnames = cx(styles['input-normal'], {
             [styles.valid]: isInputValid,
             [styles.invalid]: !isInputValid
+        });
+
+        const dateCN = cx({
+            [styles.valid]: isDateValid,
+            [styles.invalid]: !isDateValid
         })
         return (
             <form onSubmit={this.submitHandler} className={styles.container}>
@@ -85,7 +101,11 @@ class ToDoForm extends Component {
                     type="checkbox" 
                     checked={isDone} 
                     onChange={this.generalHandler}/> is done?</label>
-                <input type="datetime-local" value={format(deadline, "yyyy-MM-dd hh:mm")} onChange={this.generalHandler}/>
+                <input 
+                type="datetime-local" 
+                value={format(deadline, "yyyy-MM-dd hh:mm")} 
+                onChange={this.generalHandler}
+                className={dateCN}/>
                 <button>Submit</button>
             </form>
         );
@@ -93,3 +113,8 @@ class ToDoForm extends Component {
 }
 
 export default ToDoForm;
+
+
+/* Реалізувати валідацію інпуту з датою таким чином, щоб дедлайн таски не міг бути у минулому заздалегідь
+
+*/
