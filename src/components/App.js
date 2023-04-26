5e62292d06b2e87c2ce03d84eb80bcb1ed127c29
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-26 09:46:13 +0300

    clipboard

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,16 @@ function App (props) {
       <button onClick={() => window.location.assign('https://www.wikipedia.org/')}>Assing</button> 
       <button onClick={() => window.location.reload()}>Reload</button>
       <button onClick={() => window.location.replace('https://www.wikipedia.org')}>Replace</button>
+
+
+
+      <p>Lorem, which might be interesting <button
+                                              onClick={() => {
+                                                window.navigator.clipboard.writeText('Lorem, which might be interesting')
+                                              }}>Click to copy</button></p>
       </>
+
+
     );
 }
 
