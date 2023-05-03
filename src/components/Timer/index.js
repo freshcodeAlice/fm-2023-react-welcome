0bba2dddf7374cf2093bd64c41a241bf522d65a7
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-03 11:51:46 +0300

    Timer useEffect dependency list

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -6,6 +6,7 @@ function Timer (props) {
     const [isRunning, setRunning] = useState(true);
 
     useEffect(() => {
+        console.log('useEffect')
         if(isRunning) {
             const id = setTimeout(() => {
                 setTime(time => addSeconds(time, 1))
@@ -14,7 +15,7 @@ function Timer (props) {
                 clearTimeout(id);
             }
         }
-    })
+    }, [time, isRunning])
 
       
 
