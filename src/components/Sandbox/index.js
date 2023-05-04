97cb9da4161c323a9a443d8f5f3a6cb8204bc306
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-04 10:48:31 +0300

    useMemo

--- src/components/Sandbox/index.js
+++ src/components/Sandbox/index.js
@@ -1,39 +1,41 @@
 import ThemeContext from '../../contexts/ThemeContext';
-import React, { useCallback, useState, useEffect, useContext } from 'react';
+import React, { useCallback, useState, useEffect, useContext, useMemo } from 'react';
 
 const Sandbox = (props) => {
     const [theme, setTheme] = useContext(ThemeContext);
-    const [inputValue, setInput] = useState('1');
+    const [inputValue, setInput] = useState(1);
 
     // const changeHandler = ({ target: { value } }) => {
     //     setInput(value)
     // }
 
     const changeHandler = useCallback(({ target: { value } }) => {
-         setInput(value)
+         setInput(Number(value))
      }, []);
 
     const logValue = useCallback(() => {
         console.log(inputValue);
     }, [inputValue]);
 
-    useEffect(() => {
-        console.log('changeHandler re-created')
-       
-    }, [changeHandler]);
 
-    //   console.log('logValue changed')
-
-
-
+    const layoutValue = useMemo(() => computeValue(inputValue), [inputValue])
 
     return (
         <div>
             <input type="text" value={inputValue} onChange={changeHandler} />
             <button onClick={logValue}>Log value</button>
-            <p>{theme}</p>
+            <h1>{layoutValue}</h1>
         </div>
     );
+}
+
+
+function computeValue(value) {
+    let sum = 0;
+    for(let i = 0; i < 1000000000; i++ ){
+        sum += i;
+    }
+    return value**3;
 }
 
 export default Sandbox;
